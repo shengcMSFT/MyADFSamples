3272f17a99624b3493c98be092ae40175db4844b
Eric Chen <shengc@microsoft.com>
2017-05-11 18:59:45 +0800

    #v1

---
+++ StoredProcedureActivityforSSISPackages/Stored Procedure/USP_ExecuteSSISPackage.sql
@@ -0,0 +1,32 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		shengc
+-- Create date: 5/11/2017
+-- Description:	The sample stored procedure to call SSIS package in Synchronized mode and return failure if the package execution fails. 
+-- =============================================
+CREATE PROCEDURE USP_ExecuteSSISPackage 
+	
+	@package_name nvarchar(260), 
+	@folder_name nvarchar(128), 
+	@project_name nvarchar(128)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	    
+	DECLARE @execution_id bigint
+	DECLARE @errstr nvarchar(150)
+	EXEC [SSISDB].[catalog].[create_execution] @folder_name, @project_name, @package_name, @use32bitruntime=False, @execution_id=@execution_id OUTPUT
+	DECLARE @var0 smallint = 1
+	EXEC [SSISDB].[catalog].[set_execution_parameter_value] @execution_id,  @object_type=50, @parameter_name=N'SYNCHRONIZED', @parameter_value=@var0
+	EXEC [SSISDB].[catalog].[start_execution] @execution_id
+	IF (SELECT [status] FROM [SSISDB].[catalog].[executions] WHERE execution_id = @execution_id)<>7 -- 7 is success, returning all none success runs as error of this stored procedure execution. 
+		BEGIN
+			SET @errstr=N'The SSIS package execution with execution ID '+ CAST(@execution_id as nvarchar(20)) + N' failed. For error details, please check the SSIS catalog database.' --return execution ID for failed run
+			RAISERROR(@errstr, 15, 1)
+		END
+
+END
+GO
---
+++ StoredProcedureActivityforSSISPackages/readme.md
@@ -0,0 +1,13 @@
+This sample shows how to use Stored Procedure Activity in ADF to launch execution of an SSIS package and return result. 
+
+To use the sample:
+1. Create the stored procedure using the sample USP_ExecuteSSISPackage.sql file. 
+2. Create new ADF using the JSON definitions in the sample. This sample calls On-Premise SQL Server, so you will need to install and setup Data Management Gateway before setting up Linked Service, Dataset and Pipeline. 
+3. Update following parameters to point to your deployed SSIS package
+
+                    "storedProcedureParameters": {
+                        "package_name": "PackageName.dtsx",
+                        "folder_name": "FolderName",
+                        "project_name": "ProjectName"
+                    }
+4. The stored procedure runs SSIS package in synchronized mode, it completes only when the package execution completes. If you have a long duration package, update the timeout value to make sure it does not timeout the stored procedure. If the SSIS package completes successfully, the Stored Procedure Activity completes successful. If package fails, the stored procedur execution fails. 
